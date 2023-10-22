8f51679c76f64cf9d4579682c619806e4982cf05
Wayward Heart <91356680+WaywardHeart@users.noreply.github.com>
2023-10-22 04:58:00 -0500

    Optimize the path cache slightly
    In games that have a lot of files and an RTP is being loaded,
    this can save a couple seconds.
    
    This also keeps the cases of archives with lower priority from
    overriding the higher priority archives.

--- src/filesystem/filesystem.cpp
+++ src/filesystem/filesystem.cpp
@@ -356,6 +356,7 @@ struct CacheEnumData {
   FileSystemPrivate *p;
   std::stack<std::vector<std::string> *> fileLists;
   std::stack<std::string *> directoryList;
+  std::set<std::string> seenDirs;
 
 #ifdef __APPLE__
   iconv_t nfc2nfd;
@@ -409,6 +410,11 @@ static PHYSFS_EnumerateCallbackResult cacheEnumCB(void *d, const char *origdir,
   }
   
 
+  /* If we've already seen this mixed-case path, then we don't need to reenumerate it 
+   * A different case could have new stuff, though */
+  if (data.seenDirs.count(mixedCase))
+    return PHYSFS_ENUM_OK;
+
   /* FileSystem::normalize ensures that paths are NFD when looking for files on macOS 
    * Unfortunately, there's no guarantee the path actually is that,
    * especially when loading files from archives,
@@ -428,6 +434,9 @@ static PHYSFS_EnumerateCallbackResult cacheEnumCB(void *d, const char *origdir,
     /* Create a new list for this directory */
     std::vector<std::string> &list = data.p->fileLists[lowerCase];
 
+    /* Record that we've seen this directory */
+    data.seenDirs.insert(mixedCase);
+
     /* Iterate over its contents */
     data.fileLists.push(&list);
     lowerCase += "/";
@@ -435,7 +444,7 @@ static PHYSFS_EnumerateCallbackResult cacheEnumCB(void *d, const char *origdir,
     PHYSFS_enumerate(mixedCase.c_str(), cacheEnumCB, d);
     data.fileLists.pop();
     data.directoryList.pop();
-  } else {
+  } else if (!data.p->pathCache.contains(lowerCase)) {
     /* Get the file list for the directory we're currently
      * traversing and append this filename to it */
     std::vector<std::string> &list = *data.fileLists.top();
