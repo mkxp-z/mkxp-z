c8e5926e19d0dcf66f9f79aef486eac8b68d0f50
Francesco Corso <fcorso2016@gmail.com>
2023-10-25 22:24:17 -0400

    Changed the extconf.rb

--- extconf.rb
+++ extconf.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
-require 'mkmf'
+require "mkmf"
 
-system("cmake -S #{__dir__} -B . -D CMAKE_BUILD_TYPE=Release -G\"MinGW Makefiles\"")
+# Makes all symbols private by default to avoid unintended conflict
+# with other gems. To explicitly export symbols you can use RUBY_FUNC_EXPORTED
+# selectively, or entirely remove this flag.
+append_cflags("-fvisibility=hidden")
+
+extension_name = "mkxp_z"
+dir_config(extension_name, "#{__dir__}/include", "#{__dir__}/lib")
+have_library("mkxp_z", "initBindings")
+
+create_makefile("#{extension_name}/#{extension_name}")
