51f2010bf70bc8d9d92782aaef2e953dc4823c81
Francesco Corso <fcorso2016@gmail.com>
2023-10-29 19:22:08 -0400

    Fixed all CMake link issues

--- src/gamestate.cpp
+++ src/gamestate.cpp
@@ -63,9 +63,6 @@ extern "C" {
 __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
 __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
-
-// Windows seems to need this as well in this configuration for some reason
-#define MKXPZ_INIT_GL_LATER
 #endif
 
 #ifdef MKXPZ_STEAM
