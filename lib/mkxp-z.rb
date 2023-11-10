4faaa75a443e0e899ac11b8587b514e40a27d26c
Francesco Corso <fcorso2016@gmail.com>
2023-11-10 18:52:13 -0500

    Removed unnecessary code from the pure Ruby portion

--- lib/mkxp-z.rb
+++ lib/mkxp-z.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
 
 require_relative "mkxp-z/version"
-
-$mkxpz_launch_args ||= []
-
-module MKXP_Z
-  class Error < StandardError; end
-
-end
-
 require_relative "mkxp-z/mkxpz"
